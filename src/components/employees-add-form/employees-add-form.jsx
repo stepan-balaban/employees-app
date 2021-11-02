8099262e3e6ff923445d89a5b2be235687f3b047
Stepan Balaban <balaban.stepan@gmail.com>
2021-11-02 14:40:58 +0200

    Created a layout of app

---
+++ src/components/employees-add-form/employees-add-form.jsx
@@ -0,0 +1,19 @@
+import './employees-add-form.css';
+
+const EmployeesAddForm = () => {
+  return (
+    <div className='app-add-form'>
+      <h3>Додати нового працівника</h3>
+      <form className='add-form d-flex'>
+        <input type='text' className='form-control new-post-label' placeholder='Працівник' />
+        <input type='number' className='form-control new-post-label' placeholder='З/П в $?' />
+
+        <button type='submit' className='btn btn-outline-light'>
+          Додати
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default EmployeesAddForm;

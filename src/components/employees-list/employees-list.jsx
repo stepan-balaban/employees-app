c51be97dde844a8e89535da8bddf72c47157701c
Stepan Balaban <balaban.stepan@gmail.com>
2021-11-12 18:44:41 +0200

    Work with list items: added unique key(id) to every list item

--- src/components/employees-list/employees-list.jsx
+++ src/components/employees-list/employees-list.jsx
@@ -4,10 +4,12 @@ import './employees-list.css';
 
 const EmployeesList = ({ data }) => {
   const elements = data.map((item) => {
+    const { id, ...itemProps } = item;
     // return <EmployeesListItem name={item.name} salary={item.salary} />;
     // Або новіший запис: вивід даних через Spread оператор
-    return <EmployeesListItem {...item} />;
+    return <EmployeesListItem key={id} {...itemProps} />;
   });
+
   return <ul className='app-list list-group'>{elements}</ul>;
 };
 

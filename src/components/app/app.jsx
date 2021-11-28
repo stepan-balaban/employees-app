961645f1df6dc6ddb0f021a76c34e88d7867494b
Stepan Balaban <balaban.stepan@gmail.com>
2021-11-28 19:27:50 +0200

    added the ability to add new employees

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -18,12 +18,28 @@ class App extends Component {
         { name: 'Carl W.', salary: 5000, increase: false, id: 3 },
       ],
     };
+    this.maxId = 4;
   }
 
   deleteItem = (id) => {
     this.setState(({ data }) => {
       return {
         data: data.filter((item) => item.id !== id),
+      };
+    });
+  };
+
+  addItem = (name, salary) => {
+    const newItem = {
+      name,
+      salary,
+      increase: false,
+      id: this.maxId++,
+    };
+    this.setState(({ data }) => {
+      const newArr = [...data, newItem];
+      return {
+        data: newArr,
       };
     });
   };
@@ -39,7 +55,7 @@ class App extends Component {
         </div>
 
         <EmployeesList data={this.state.data} onDelete={this.deleteItem} />
-        <EmployeesAddForm />
+        <EmployeesAddForm onAdd={this.addItem} />
       </div>
     );
   }
--- src/components/employees-add-form/employees-add-form.jsx
+++ src/components/employees-add-form/employees-add-form.jsx
@@ -17,15 +17,38 @@ class EmployeesAddForm extends Component {
     });
   };
 
+  onSubmit = (e) => {
+    e.preventDefault();
+    this.props.onAdd(this.state.name, this.state.salary);
+    this.setState({
+      name: '',
+      salary: '',
+    });
+  };
+
   render() {
     const { name, salary } = this.state;
 
     return (
       <div className='app-add-form'>
         <h3>Додати нового працівника</h3>
-        <form className='add-form d-flex'>
-          <input type='text' className='form-control new-post-label' placeholder='Працівник' name='name' value={name} onChange={this.onValueChange} />
-          <input type='number' className='form-control new-post-label' placeholder='З/П в $?' name='salary' value={salary} onChange={this.onValueChange} />
+        <form className='add-form d-flex' onSubmit={this.onSubmit}>
+          <input
+            type='text'
+            className='form-control new-post-label'
+            placeholder='Працівник'
+            name='name'
+            value={name}
+            onChange={this.onValueChange}
+          />
+          <input
+            type='number'
+            className='form-control new-post-label'
+            placeholder='З/П в $?'
+            name='salary'
+            value={salary}
+            onChange={this.onValueChange}
+          />
           <button type='submit' className='btn btn-outline-light'>
             Додати
           </button>

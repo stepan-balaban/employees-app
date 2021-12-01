3193c656ac79c2672ebf937fbcf4a961fcccf3ae
Stepan Balaban <balaban.stepan@gmail.com>
2021-12-01 11:00:33 +0200

    added search functionality

--- src/components/search-panel/search-panel.jsx
+++ src/components/search-panel/search-panel.jsx
@@ -1,7 +1,32 @@
+import { Component } from 'react';
+
 import './search-panel.css';
 
-const SearchPanel = () => {
-  return <input type='text' className='form-control search-input' placeholder='Знайти працівника' />;
-};
+class SearchPanel extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      term: '',
+    };
+  }
+
+  onUpdateSearchLocal = (e) => {
+    const localTerm = e.target.value;
+    this.setState({ term: localTerm });
+    this.props.onUpdateSearch(localTerm);
+  };
+
+  render() {
+    return (
+      <input
+        type='text'
+        className='form-control search-input'
+        placeholder='Знайти працівника'
+        value={this.state.term}
+        onChange={this.onUpdateSearchLocal}
+      />
+    );
+  }
+}
 
 export default SearchPanel;
